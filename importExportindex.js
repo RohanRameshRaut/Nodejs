299c5b959c729c86f043214f8d1abc1e3cff13eb
RohanRameshRaut <rohanraut918@gmail.com>
2024-04-07 17:07:18 +0530

    Update importExportindex.js

--- importExportindex.js
+++ importExportindex.js
@@ -8,6 +8,6 @@ const {add, sub, mult, name} = require('./oper'); //object based structure
 console.log(add(5, 5));
 console.log(sub(10, 5));
 console.log(mult(10, 5));
-console.log(name);
+console.log5(name);
 
 
